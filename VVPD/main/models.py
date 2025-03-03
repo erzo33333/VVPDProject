43ed0b712276ae4fb1cf97df6b07b8456c073601
erzo333 <2006temih@gmail.com>
2025-03-03 21:12:59 +0700

    Добавил модель Event в бд.

--- VVPD/main/models.py
+++ VVPD/main/models.py
@@ -7,3 +7,13 @@ class User(models.Model):
     password = models.CharField(max_length=50, blank=False)
     EventsID = models.JSONField(null=True, blank=True, default=[])
     FriendsID = models.JSONField(null=True, blank=True, default=[])
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    creatorID = models.CharField(max_length=200)
+    startTime = models.DateTimeField(blank=False)
+    endTime = models.DateTimeField(blank=False)
+    description = models.JSONField(null=True, blank=True, default=[])
+    colour = models.CharField(max_length=20, blank=True, default='grey')
+    participants = models.JSONField(blank=True, default=[]) #надо не забыть добавить метод добавления сюда creatorID при создании объекта
