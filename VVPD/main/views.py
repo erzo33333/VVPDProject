9222cf5214d53ca16ba136b44076bc7e05309ce0
nikit <qweqw@aefd>
2025-03-04 10:54:59 +0700

    Вернул тестовую ветку до состояния master

--- VVPD/main/views.py
+++ VVPD/main/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from uuid import uuid4
 
+
 def index_page(request):
     return render(request, 'indexPage.html')
+
+def second_page(request):
+    return render(request, 'SecondPage.html')
