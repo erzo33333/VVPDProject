909accc72f284310d2d35348885da92dbb2d3f75
NPlatonovv <pl_an777@list.ru>
2025-04-10 12:43:48 +0700

    Кривая косая регистрация

--- VVPD/main/urls.py
+++ VVPD/main/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.user_logout, name='logout'),
+    path('', views.main_page, name='main page'),
+    path('second/', views.second_page, name='second'),
 ]
--- VVPD/main/views.py
+++ VVPD/main/views.py
@@ -2,13 +2,45 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
 from main.models import User, Event
-
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, RegistrationForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('main page')
+            else:
+                return render(request, 'login.html', {'form': form, 'error': 'Неверные учетные данные'})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+@login_required(login_url='login')
 def main_page(request):
     return render(request, 'MainPage.html', context={})
-
+@login_required(login_url='login')
 def index_page(request):
     return render(request, 'IndexPage.html', context={})
-
+#@login_required(login_url='login')
 def second_page(request):
     return render(request, 'SecondPage.html', context={})
